9c663f368eac896235ed1b88747539821f335b25
Kev <bichito03052005@gmail.com>
2023-08-27 19:35:56 -0500

    Conf(BackEnd): Add Middleware, Mapwhen y MapBranch, DBContext, Cargo Entity, RepositoryManager, LoggerService

--- APIMueblesJYD/APIRestProyecto/Program.cs
+++ APIMueblesJYD/APIRestProyecto/Program.cs
@@ -1,4 +1,17 @@
+using Contracts;
+using APIRestProyecto.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+//using Microsoft.EntityFrameworkCore;
+using NLog;
+//using Repository;
+
 var builder = WebApplication.CreateBuilder(args);
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+"/nlog.config"));
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 
 // Add services to the container.
 
@@ -6,11 +19,63 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+} else
+{
+    app.UseHsts();
+}
+
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logica antes de ejecutar el siguiente delegado en el metodo use");
+    await next.Invoke();
+    Console.WriteLine($"Logica despues de ejecutar el delegado en el metodo use");
+});
+
+app.Map("/usingmapbranch", builder =>
+{
+    builder.Use(async (context, next) =>
+    {
+        Console.WriteLine("Map branch Logica antes de ejecutar el siguiente delegado en el metodo use");
+        await next.Invoke();
+        Console.WriteLine("Map branch Logica despues de ejecutar el siguiente delegado en el metodo use");
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"Map branch Respuesta al cliente en el metodo Run");
+        await context.Response.WriteAsync("Bienvenido al MapBranch");
+    });
+});
+
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
+    {
+        builder.Run(async context =>
+    {
+        await context.Response.WriteAsync("Bienvenido a la rama del MapWhen");
+    });
+});
+
+app.Run(async context =>
+{
+    Console.WriteLine($"Escribiendo la respuesta al cliente desde el metodo Run");
+    await context.Response.WriteAsync("Bienvenido al componente delñ Middleware");
+});
 
 app.MapControllers();
 
---
+++ APIMueblesJYD/Repository/Configuration/CargoConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Repository.Configuration
+{
+    public class CargoConfiguration : IEntityTypeConfiguration<Cargo>
+    {
+        public void Configure(EntityTypeBuilder<Cargo> builder)
+        {
+            builder.HasData
+            (
+            new Cargo
+            {
+                IdCargo = new Guid("81ef2c34-7eb7-4891-8e5b-172e5786e687"),
+                NombreCargo = "Carpintero",
+                Estado = 1,
+            },
+            new Cargo
+            {
+                IdCargo = new Guid("2d2df54e-e3ff-45e3-915d-ac2f53d371f2"),
+                NombreCargo = "Asistente de carpinteria",
+                Estado = 1
+            }
+            );
+        }
+    }
+}
