3740d9e96e8176bf41bdd4665e83bfedf2c493a1
Kev <bichito03052005@gmail.com>
2023-08-31 22:42:55 -0500

    Conf(Backend): Add EntitiesDTO and IEntitiesRepository

---
+++ APIMueblesJYD/Shared/DataTransferObjects/DFacturaVentaDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    public record DFacturaVentaDTO (Guid Id, float ValorUnitario, int Cantidad, float IVA, 
+        float ValorDescuento, Guid IdProducto, Guid IdFacturaVenta);
+    
+}
---
+++ APIMueblesJYD/Shared/DataTransferObjects/FacturaVentaDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    public record FacturaVentaDTO(Guid IdFacturaVenta, int NFactura, DateTime FechaGeneracion, DateTime FechaExpedicion, 
+        DateTime FechaVencimiento, float TotalBruto, float TotalIVA, float TotalRefuete, float TotalPago, Guid IdUsuario);
+
+}
